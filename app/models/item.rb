63d8da53a655c9adb2f8292dd05ea997682fd3e9
EC2 Default User <k9batai33@gmail.com>
2022-07-18 09:02:16 +0000

    Fix

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,11 +9,11 @@ class Item < ApplicationRecord
   validates :price, presence: true
   validates :is_active, inclusion: {in: [true, false]}
 
-  def get_image
+  def get_image(width,height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    image.variant(resize_to_limit: [width, height]).processed
   end
 end
