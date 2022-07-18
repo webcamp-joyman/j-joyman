ed0ecbc4fa9adfff0f01a31ddd1a46b100a99aaa
EC2 Default User <ec2-user@ip-172-31-11-35.ap-northeast-1.compute.internal>
2022-07-18 09:13:12 +0000

    confirm

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,11 +9,11 @@ class Item < ApplicationRecord
   validates :price, presence: true
   validates :is_active, inclusion: {in: [true, false]}
 
-  def get_image(width,height)
+  def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image.variant(resize_to_limit: [width, height]).processed
+    image
   end
 end
