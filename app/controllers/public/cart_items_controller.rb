ea6f9fe9f6fa5d49fd4af4b158da192659263de8
Kyohei Sasaki <106052021+kyohei960@users.noreply.github.com>
2022-07-18 14:28:43 +0000

    [Start]カート内商品作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,14 @@
 class CartItemsController < ApplicationController
+  def index
+    @cart_item = CartItem.find()
+  end
+  
+  def create
+  end
+  
+  def destroy
+  end
+  
+  def update
+  end
 end
