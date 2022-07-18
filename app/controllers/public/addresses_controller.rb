b288ac8175703befeea8623ae6d48577f432d27a
EC2 Default User <ec2-user@ip-172-31-11-35.ap-northeast-1.compute.internal>
2022-07-18 04:29:12 +0000

    init

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,45 @@
 class Public::AddressesController < ApplicationController
+  before_action :authenticate_member!
+
+  def index
+    @address = Address.new
+    @addresses = current_member.addresses
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.member_id = current_member.id
+    if @address.save
+      redirect_to addresses_path
+    else
+      @addresses = current_member.addresses
+      render :index
+    end
+  end
+
+  def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
+  end
+
+
+  private
+
+  def address_params
+    params.require(:address).permit(:post_code, :address, :name)
+  end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :member
+
+  validates :post_code, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
 end
