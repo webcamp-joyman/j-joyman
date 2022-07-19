1a14587b5584ae7231eaefd9b5a676324fed231b
Kyohei Sasaki <106052021+kyohei960@users.noreply.github.com>
2022-07-19 01:12:54 +0000

    [Update]カート内商品

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,14 +1,21 @@
 class CartItemsController < ApplicationController
   def index
-    @cart_item = CartItem.find()
+    @cart_items = CartItem.all
+    @cart_item.member_id = current_user
   end
-  
+
   def create
   end
-  
+
   def destroy
   end
-  
+
   def update
+  end
+
+  private
+
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
   end
 end
