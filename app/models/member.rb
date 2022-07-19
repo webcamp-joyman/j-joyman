3f3b0aa93d18b484f71d8b857f6a8348f6535349
mrtknj <105144477+mrtknj@users.noreply.github.com>
2022-07-19 03:09:22 +0000

    顧客新規登録途中

--- app/models/member.rb
+++ app/models/member.rb
@@ -8,4 +8,11 @@ class Member < ApplicationRecord
   has_many :cart_items
   has_many :addresses
   
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :call_number, presence: true
+  validates :post_code, presence: true
+  validates :address, presence: true
 end
