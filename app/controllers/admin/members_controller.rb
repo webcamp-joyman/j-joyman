1d2b48252f08e84aca51d4230883b673b48e2c3a
EC2 Default User <ec2-user@ip-172-31-11-35.ap-northeast-1.compute.internal>
2022-07-19 09:23:44 +0000

    Add admin_members_edit

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -10,4 +10,25 @@ class Admin::MembersController < ApplicationController
 
   end
 
+  def edit
+    @member = Member.find(params[:id])
+  end
+
+  def update
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      redirect_to admin_member_path(@member.id)
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def member_params
+    params.require(:member).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :call_number, :email, :is_valid)
+  end
+
+
 end
