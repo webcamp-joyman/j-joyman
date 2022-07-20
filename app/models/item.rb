39561c093b7bba87c592dd87138f051f345b273b
EC2 Default User <k9batai33@gmail.com>
2022-07-20 09:01:51 +0000

    途中

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,4 +16,9 @@ class Item < ApplicationRecord
     end
     image.variant(resize_to_limit: [width, height]).processed
   end
+  
+  def add_tax_price
+    (self.price * 1.10).round
+  end
+  
 end
