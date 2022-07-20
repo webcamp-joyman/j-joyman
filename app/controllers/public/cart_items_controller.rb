25e5d56366806b7b0bf0d53262422db58d0a5308
Kyohei Sasaki <106052021+kyohei960@users.noreply.github.com>
2022-07-20 06:20:35 +0000

    カート内商品ほぼ完成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,20 +1,26 @@
-class CartItemsController < ApplicationController
+class Public::CartItemsController < ApplicationController
+
   def index
-    @cart_items = current_member.cart_items.all
+    @cart_items = CartItem.all
     @total = 0
   end
 
   def create
-    @cart_item = current_member.cart_items.new(cart_item_params)
-    if current_member.cart_items.find_by(item_id: params[:cart_item][:item_id].present)
-      cart_item = current_member.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    unless member_signed_in?
+      redirect_to items_path
+    else
+      @cart_items = current_member.cart_items.new(cart_item_params)
+      if @cart_items.save
+        redirect_to cart_items_path
+      else
+        redirect_to item_path(@cart_items.item_id)
+      end
     end
   end
 
   def destroy
-    cart_item = CartItem.find(params[:id])
-    cart_item.destroy
-    @cart_items = CartItem.all
+    @cart_items = CartItem.find(params[:id])
+    @cart_items.destroy
     redirect_to cart_items_path
   end
 
@@ -25,8 +31,8 @@ class CartItemsController < ApplicationController
   end
 
   def update
-    @cart_item = CartItem.find(params[:id])
-    @cart_item.update(cart_item_params)
+    @cart_items = CartItem.find(params[:id])
+    @cart_items.update(cart_item_params)
     redirect_to cart_items_path
   end
 
