0ae0fa38a1c022476f065bc676d66771197465a8
EC2 Default User <k9batai33@gmail.com>
2022-07-20 02:39:01 +0000

    途中

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,5 +3,10 @@ class Public::ItemsController < ApplicationController
   def index
     @items = Item.all.page(params[:page]).per(10)
   end
+  
+  def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
+  end  
 
 end
