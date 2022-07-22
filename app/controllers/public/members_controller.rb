78f486b97250c91678f3dc0e3f7dd3bc1a351602
EC2 Default User <ec2-user@ip-172-31-11-35.ap-northeast-1.compute.internal>
2022-07-22 06:15:55 +0000

    Add unsubscribe/view

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -9,7 +9,7 @@ class Public::MembersController < ApplicationController
   def edit
     @member = current_member
   end
-  
+
   def update
     member = current_member
     if member.update(member_params)
@@ -18,17 +18,25 @@ class Public::MembersController < ApplicationController
       render :edit
     end
   end
-     
+
   def unsubscribe
-    
+    @member = Member.find_by(email: params[:email])
   end
-  
-  
+
+
+  def withdraw
+    @member = Member.find_by(email: params[:email])
+    @member.update(is_valid: false)
+    reset_session
+    redirect_to root_path
+  end
+
+
   private
-  
+
   def member_params
     params.require(:member).permit(:last_name, :first_name, :last_name_kana, :first_name_name, :post_code, :address, :call_number, :email)
   end
-  
+
 
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :member_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -24,4 +25,17 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  protected
+
+  def  member_state
+    @member = Member.find_by(email: params[:member][:email])
+    if @member
+      if @member.valid_password?(params[:member][:password] && !@member.is_valid)
+        redirect_to new_member_registration_path
+      end
+    end
+  end
+
+
 end
