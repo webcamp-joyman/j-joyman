7e54f7b8c438ce63b479c4d6e8c7edbde49fc4ce
mrtknj <105144477+mrtknj@users.noreply.github.com>
2022-07-24 08:03:54 +0000

    アドミン注文詳細途中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,10 @@
 class Admin::OrdersController < ApplicationController
+  def show
+   @order = Order.where(id: params[:id])
+  end
+  
+  private
+    def order_params
+      params.require(:order).permit(:order_status)
+    end
 end
