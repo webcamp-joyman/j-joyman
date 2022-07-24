1bf818d53ecb3d2ddf93dcd4c466a89d5f5b07e2
mrtknj <105144477+mrtknj@users.noreply.github.com>
2022-07-24 14:19:33 +0000

    注文履歴詳細完成

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,18 @@
 class Admin::OrderDetailsController < ApplicationController
+  def update
+    order_detail = OrderDetail.find(params[:id])
+    order_detail.update(order_detail_params)
+    order = order_detail.order
+    order_details = order.order_details
+    
+    flash[:notice] = "製作ステータスが更新されました"
+    redirect_to admin_order_path(order)
+  end
+  
+  private
+  
+    def order_detail_params
+      params.require(:order_detail).permit(:making_status)
+    end
+    
 end
