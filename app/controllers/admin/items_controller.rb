6221d404dee1de03c6ee7a5248ed0bf615cb4013
EC2 Default User <k9batai33@gmail.com>
2022-07-25 06:41:27 +0000

    修正

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -9,7 +9,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(admin_item_params)
     if @item.save
-      redirect_to admin_items_path
+      redirect_to admin_item_path(@item.id)
     else
       @genres = Genre.all
       render :new
