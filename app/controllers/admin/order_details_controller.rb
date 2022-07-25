7565f540d253bfd5d7c2b75d82420edea2d9bbfa
mrtknj <105144477+mrtknj@users.noreply.github.com>
2022-07-25 08:45:41 +0000

    製作ステータス注文ステータス修正

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -4,9 +4,27 @@ class Admin::OrderDetailsController < ApplicationController
     order_detail.update(order_detail_params)
     order = order_detail.order
     order_details = order.order_details
+    order_details.each do |order_detail|
+      if order_detail.making_status == "製作中"
+        order.status = "製作中"
+        order.save
+      end
+    end
+    
+    var = 0
+    order_details.each do |order_detail|
+      if order_detail.making_status == "製作完了"
+        var += 1
+      end
+    end
+    if var == order_details.count
+      order.status = "発送準備中"
+      order.save
+    end
+      
     
     flash[:notice] = "製作ステータスが更新されました"
-    redirect_to admin_order_path(order)
+    redirect_to request.referer
   end
   
   private
